c6b16be2e60443112c3b898ff3cd5acc0a4d1b70
Nations <mohlalanation7@gmail.com>
2023-02-16 05:23:36 -0800

    task 5 a C program that prints exactly with proper grammar, but the outcome is a piece of art,

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,13 @@
-#include<stdio.h>
+#include <stdio.h>
+
 /**
- *main - A program that print a line using the printf function
- *Return: 0 (Success)
+ * main - prints "with proper grammar, but the outcome is a piece of art," followed newline
+ * Return: Always 0.
+ *
  */
+
 int main(void)
+
 {
-printf("with proper grammar, but the outcome is a piece of art,\n")
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
-}	
