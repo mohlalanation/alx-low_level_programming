f11273e2106740a1b46c1ad6c8c3819d90e37920
Nations <mohlalanation7@gmail.com>
2023-02-16 10:51:20 -0800

    task 5 printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-
 /**
- * main - prints "with proper grammar, but the outcome is a piece of art," followed newline
- * Return: Always 0.
+ * main - Entry point
  *
+ * Return: Always 0 (Success)
  */
-
 int main(void)
-
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
